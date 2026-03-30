4fc7f135052332bccf4d099f9784935da20c6c02
unknown <arshitha0987@gmail.com>
2026-03-30 15:55:17 +0530

    Fixed app init logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,12 @@
-# This file makes the 'app' folder a Python package
+from flask import Flask
+from .models import db 
+from .routes import main
+
+def create_app():
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    
+    db.init_app(app) 
+    app.register_blueprint(main)
+    
+    return app
