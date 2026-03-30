441af48743c2cf0562cb79bc51b808d0bb22d716
unknown <arshitha0987@gmail.com>
2026-03-30 16:26:53 +0530

    Local sync

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from .routes import main
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app) 
     app.register_blueprint(main)
